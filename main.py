d2601ceea2b2d08ac53891be8a93454c08fd3324
tphsmp <40296154+tphsmp@users.noreply.github.com>
2022-06-07 16:56:59 +0600

    Update main.py

--- main.py
+++ main.py
@@ -32,9 +32,8 @@ def buttons_actions(message):
     if(message.text == 'One_rune'):
         bot.send_message(message.chat.id, text="Руня дня " + ranumber())
     elif (message.text == 'Three_runes'):
-        bot.send_message(message.chat.id, text="Три руны" + ranumber())
-        bot.send_message(message.chat.id, text="Три руны" + ranumber())
-        bot.send_message(message.chat.id, text="Три руны" + ranumber())
+        for i in range(3):
+            bot.send_message(message.chat.id, text="Три руны ")
     else:
         pass
         
