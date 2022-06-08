c09d02c865e8a2b7e014a0b2adf2f81891b0069e
tphsmp <40296154+tphsmp@users.noreply.github.com>
2022-06-08 08:24:53 +0600

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def buttons_actions(message):
         pass
         
 
-@server.route('/' + TOKEN, methods=['POST', 'GET'])
+@server.route('/' + TOKEN, methods=['POST'])
 def get_message():
     json_string = request.get_data().decode('utf-8')
     update = telebot.types.Update.de_json(json_string)
